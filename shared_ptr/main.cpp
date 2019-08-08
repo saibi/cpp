8ebb0b87064521fbead09661b441c8ac86447114
Youngmin Kim <ymkim@huvitz.com>
2019-08-08 09:50:30 +0900

    unique_ptr

--- shared_ptr/main.cpp
+++ shared_ptr/main.cpp
@@ -20,12 +20,17 @@ int main()
 	cout << "*s = " << *s << endl;
 	cout << "*t = " << *t << endl;
 
-	shared_ptr<int> u = t;
-	cout << "ref count = 3" << endl;
-	*u += 1;
-	cout << "*s = " << *s << endl;
-	cout << "*t = " << *t << endl;
-	cout << "*u = " << *u << endl;
+
+
+	unique_ptr<int> u(new int);
+	*u = 18;
+
+
+	// compile-time error
+	//unique_ptr<int> v = u;
+	//s = u 
+	//u = s
+
 
 	return 0;
 }
