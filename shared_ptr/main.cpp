753639ae77078dde6e329fd1f141c70b17badfaa
Youngmin Kim <ymkim@huvitz.com>
2019-08-08 10:23:50 +0900

    use_count()

--- shared_ptr/main.cpp
+++ shared_ptr/main.cpp
@@ -9,14 +9,15 @@ int main()
 
 	*s = 10;
 
-	cout << "ref count = 1" << endl;
+	cout << "ref count = " << s.use_count() << endl;
 	cout << "*s = " << *s << endl;
 	
 	shared_ptr<int> t = s;
 
 	*t += 1;
 
-	cout << "ref count = 2" << endl;
+	cout << "ref count = " << s.use_count() << endl;
+	cout << "ref count = " << t.use_count() << endl;
 	cout << "*s = " << *s << endl;
 	cout << "*t = " << *t << endl;
 
